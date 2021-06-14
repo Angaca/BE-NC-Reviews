350e9195c4aaa42dd2b51c23928d05778f112e44
Andrea Catania <andreacatania91@gmail.com>
2021-06-14 16:19:01 +0100

    start implementing app and first endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,27 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/categories", () => {
+  test("GET - status 200 - should return an object with an array of categories", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.categories).toHaveLength(4);
+        body.categories.forEach((category) => {
+          expect(category).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
