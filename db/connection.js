e6845a96521df9e8a994299a2f57b7c3120c8dc9
Andrea Catania <andreacatania91@gmail.com>
2021-06-17 09:56:38 +0100

    changed few lines in connection and seeded again production db

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,6 @@ const { Pool } = require("pg");
 const path = require("path");
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({
-  path: path.resolve(__dirname, `../.env.${ENV}`),
-});
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE OR DATABASE_URL not set");
-}
 const config =
   ENV === "production"
     ? {
@@ -18,5 +11,13 @@ const config =
         },
       }
     : {};
+
+require("dotenv").config({
+  path: path.resolve(__dirname, `../.env.${ENV}`),
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE OR DATABASE_URL not set");
+}
 
 module.exports = new Pool();
